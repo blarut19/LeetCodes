f09e980be274ba15f659e31492a4b4affe9fd873
blarut19 <49119532+blarut19@users.noreply.github.com>
2024-07-07 13:11:58 +0200

    Time: 48 ms (72.88%), Space: 48.8 MB (28.81%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} numBottles
+ * @param {number} numExchange
+ * @return {number}
+ */
+var numWaterBottles = function(numBottles, numExchange) {
+    if(numBottles < numExchange)
+        return numBottles;
+    let undrunk = numBottles%numExchange
+    return Math.floor(numBottles - undrunk) + numWaterBottles(Math.floor(numBottles/numExchange) + undrunk, numExchange);
+};
