31000e28a3adefb6b06d4e9efa9e057a64b92add
blarut19 <49119532+blarut19@users.noreply.github.com>
2024-11-11 12:54:38 +0100

    Time: 3 ms (67.88%), Space: 27.5 MB (78.91%) - LeetHub

---
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool primeSubOperation(vector<int>& nums) {
+        // List of primes up to 1009
+        int primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009};
+        int prev = 0;
+        for(int num : nums) {
+            // Case for when previous number is 1 less than current
+            if(num-1 == prev || num-2 == prev) {
+                prev = num;
+                continue;
+            }
+            // Check all primes to find the largest possible one to substract
+            int i = 0;
+            while((num - primes[i]) > prev && i <= 168)
+                i++;
+            // Case for when previous number is strictly larger than current or when we fall out of range
+            if(i == 0 || i > 168)
+                return false;
+            prev = num - primes[i-1];
+        }
+        return true;
+    }
+};
